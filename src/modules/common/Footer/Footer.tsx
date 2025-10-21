15af4156ce224d2a7a13a785803deb3db21d0242
Yaroslav Khakhuda <hahyda14@gmail.com>
2025-10-22 00:22:17 +0300

    Refactor code structure for improved readability and maintainability

--- src/modules/common/Footer/Footer.tsx
+++ src/modules/common/Footer/Footer.tsx
@@ -1,5 +1,11 @@
 import { Menu } from '@/components/nav'
 import { useTranslation } from 'react-i18next'
+import Tel from '@/assets/icons/tel.svg?react'
+import Email from '@/assets/icons/email.svg?react'
+import Facebook from '@/assets/icons/facebook.svg?react'
+import Instagram from '@/assets/icons/instagram.svg?react'
+import Youtube from '@/assets/icons/youtube.svg?react'
+import Tiktok from '@/assets/icons/tiktok.svg?react'
 const NAV_ITEMS = [
   { label: 'aboutPegasus', href: '/about-pegasus' },
   { label: 'systems', href: '/systems' },
@@ -38,69 +44,74 @@ export const Footer = () => {
           </a>
         </p>
       </div>
-      <div className="flex justify-between">
+      <div className="flex justify-between w-full">
         <Menu
           navItems={NAV_ITEMS}
           ariaLabel={t('footer.navigationAriaLabel')}
           className="flex-col gap-4"
         />
-        <div>
-          <ul className="text-white font-medium text-[20px] uppercase">
+        <div className="flex flex-col justify-between">
+          <ul className="text-white font-medium text-[20px] uppercase flex flex-col gap-6">
             <li>
               <a
+                className="flex gap-3"
                 type="tel"
                 target="_blank"
                 href="tel:+380986146177"
                 rel="nofollow"
               >
-                +380 (98) 614 61 77
+                <Tel width={32} height={32} /> +380 (98) 614 61 77
               </a>
             </li>
             <li>
-              <a type="email" href="mailto:p.info@pegasusarms.com.ua">
-                p.info@pegasusarms.com.ua
+              <a
+                className="flex gap-3"
+                type="email"
+                href="mailto:p.info@pegasusarms.com.ua"
+              >
+                <Email width={32} height={32} /> p.info@pegasusarms.com.ua
               </a>
             </li>
           </ul>
-          <ul className="text-white">
-            <li>
+          <ul className="text-white flex gap-3">
+            <li className="border rounded-[4px]">
               <a
                 aria-label="facebook"
                 target="_blank"
                 rel="nofollow noopener"
                 href="https://www.facebook.com/profile.php?id=61559913531013&locale=uk_UA"
               >
-                facebook
+                <Facebook width={42} height={42} />
               </a>
             </li>
-            <li>
+            <li className="border rounded-[4px]">
               <a
                 aria-label="instagram"
                 target="_blank"
                 rel="nofollow noopener"
                 href="https://www.instagram.com/drone.pegasusarms?igsh=MXVyYzV3aGVlbWRoNg%3D%3D&utm_source=qr"
               >
-                instagram
+                <Instagram width={42} height={42} />
               </a>
             </li>
-            <li>
+            <li className="border rounded-[4px]">
               <a
                 aria-label="youtube"
                 target="_blank"
                 rel="nofollow noopener"
                 href="https://youtube.com/@pegasusarmsukraine?si=phZUjIuPs0lCvMd_"
               >
-                youtube
+                <Youtube width={42} height={42} />
               </a>
             </li>
-            <li>
+            <li className="border rounded-[4px]">
               <a
                 aria-label="tiktok"
                 target="_blank"
                 rel="nofollow noopener"
                 href="https://www.tiktok.com/@drone.pegasusarms?_t=ZM-8vlt5EPwJHD"
               >
-                tiktok
+                <Tiktok width={42} height={42} />
               </a>
             </li>
           </ul>
