8334a1490938875675ec3bf7b102ae935ff1c7f7
Yaroslav Khakhuda <hahyda14@gmail.com>
2025-10-22 14:58:10 +0300

    feat: add HeroSection component for About Pegasus page
    - Implemented HeroSection with translation support using react-i18next
    - Added styled text elements and an image to enhance the section's visual appeal
    - Created an index file for easier imports of the HeroSection component

---
+++ src/modules/aboutPegasus/HeroSection/HeroSection.tsx
@@ -0,0 +1,35 @@
+import { SectionContainer } from '@/components/container/SectionContainer/SectionContainer'
+import { SectionTitle } from '@/components/text'
+import { useTranslation } from 'react-i18next'
+
+export const HeroSection = () => {
+  const { t } = useTranslation()
+  return (
+    <SectionContainer
+      as="section"
+      className="flex pt-[139px] flex-col justify-center items-center"
+    >
+      <div className="w-full flex flex-col gap-6">
+        <p
+          className="font-light bg-[linear-gradient(90.79deg,#F5F5F5_0.08%,#FDFFFF_100%)] bg-clip-text text-transparent uppercase tex-[32px]
+"
+        >
+          {t('navMain.aboutPegasus')}
+        </p>
+        <SectionTitle
+          title={`// ${t('heroAboutPegasus.title')}`}
+          className="text-left w-full"
+        />
+        <p className="font-light uppercase max-w-[1011px] bg-[linear-gradient(90.79deg,#F5F5F5_0.08%,#FDFFFF_100%)] bg-clip-text text-transparent text-[24px]">
+          {t('heroAboutPegasus.paragraphs.0')}
+        </p>
+      </div>
+      <img src="plug.png" className="my-[82px]" />
+      <div className="w-full flex justify-end">
+        <p className="font-light uppercase max-w-[1011px] bg-[linear-gradient(90.79deg,#F5F5F5_0.08%,#FDFFFF_100%)] bg-clip-text text-transparent text-[24px]">
+          {t('heroAboutPegasus.paragraphs.1')}
+        </p>
+      </div>
+    </SectionContainer>
+  )
+}
--- src/pages/AboutPegasusPage/AboutPegasusPage.tsx
+++ src/pages/AboutPegasusPage/AboutPegasusPage.tsx
@@ -1,8 +1,10 @@
+import { HeroSection } from '@/modules/aboutPegasus/HeroSection'
 import { DevelopSection } from '@/modules/common/DevelopSection'
 
 export const AboutPegasusPage = () => {
   return (
     <main>
+      <HeroSection />
       <DevelopSection />
     </main>
   )
