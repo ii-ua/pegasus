cf5427a9665707b6ecfcd562b789db6f1106e71f
Yaroslav Khakhuda <hahyda14@gmail.com>
2025-10-28 20:53:48 +0200

    Refactor code structure for improved readability and maintainability

--- src/pages/AboutPegasusPage/AboutPegasusPage.tsx
+++ src/pages/AboutPegasusPage/AboutPegasusPage.tsx
@@ -1,3 +1,4 @@
+import { MainContainer } from '@/components/container'
 import { EcosystemSection } from '@/modules/aboutPegasus/EcosystemSection'
 import { HeroSection } from '@/modules/aboutPegasus/HeroSection'
 import { OursSection } from '@/modules/aboutPegasus/OursSection'
@@ -5,11 +6,13 @@ import { DevelopSection } from '@/modules/common/DevelopSection'
 
 export const AboutPegasusPage = () => {
   return (
-    <main>
-      <HeroSection />
-      <OursSection />
-      <EcosystemSection />
-      <DevelopSection />
+    <main className="about-page-bg">
+      <MainContainer>
+        <HeroSection />
+        <OursSection />
+        <EcosystemSection />
+        <DevelopSection />
+      </MainContainer>
     </main>
   )
 }
--- src/pages/systems/BplaPage/BplaPage.tsx
+++ src/pages/systems/BplaPage/BplaPage.tsx
@@ -1,14 +1,17 @@
 import { FormSection } from '@/modules/common/FormSection'
 import { BplaHero, ResultsSection } from '@/modules/systems/bpla'
 import { IntegrationSection } from '@/modules/common/IntegrationSection'
+import { MainContainer } from '@/components/container'
 
 export const BplaPage = () => {
   return (
-    <main>
-      <BplaHero />
-      <ResultsSection />
-      <IntegrationSection />
-      <FormSection />
+    <main className="bpla-page-bg">
+      <MainContainer>
+        <BplaHero />
+        <ResultsSection />
+        <IntegrationSection />
+        <FormSection />
+      </MainContainer>
     </main>
   )
 }
