a95f38a0c52774828837ebc1d4a5e7f223c88a51
Yaroslav Khakhuda <hahyda14@gmail.com>
2025-11-17 16:58:18 +0200

    Refactor code structure for improved readability and maintainability

--- src/components/text/SectionTitle/SectionTitle.tsx
+++ src/components/text/SectionTitle/SectionTitle.tsx
@@ -1,3 +1,5 @@
+import { cn } from '@/common/utils/cn'
+
 export interface SectionTitleProps {
   title: string
   className?: string
@@ -5,7 +7,10 @@ export interface SectionTitleProps {
 export const SectionTitle = ({ title, className }: SectionTitleProps) => {
   return (
     <h3
-      className={`font-[Univermag] font-normal text-[48px]  tablet:text-[72px] desktop:text-[90px] text-[#FDFFFF] uppercase ${className}`}
+      className={cn(
+        'font-[Univermag] font-normal text-[48px]  tablet:text-[72px] desktop:text-[90px] text-[#FDFFFF] uppercase',
+        className,
+      )}
     >
       {title}
     </h3>
--- src/modules/systems/bpla/BplaHero/BplaHero.tsx
+++ src/modules/systems/bpla/BplaHero/BplaHero.tsx
@@ -1,6 +1,6 @@
 import { StatCard } from '@/components/cards'
 import { SectionContainer } from '@/components/container'
-import { SectionTitle } from '@/components/text'
+import { Paragraph, SectionTitle } from '@/components/text'
 import { useTranslation } from 'react-i18next'
 import Mina from '@/assets/icons/mina.svg?react'
 import Munitions from '@/assets/icons/munitions.svg?react'
@@ -11,31 +11,49 @@ export const BplaHero = () => {
   return (
     <SectionContainer
       as="section"
-      className="flex pt-[90px] tablet:pt-[64px] flex-col justify-center items-center gap-[42px]"
+      className="flex pt-[90px] tablet:pt-[64px] flex-col justify-center items-center gap-[54px] tablet:gap-[64px] desktop:gap-[82px]"
     >
-      <div className="w-full flex flex-col gap-6">
-        <p
-          className="font-light bg-[linear-gradient(90.79deg,#F5F5F5_0.08%,#FDFFFF_100%)] bg-clip-text text-transparent uppercase tex-[32px]
-  "
-        >
-          {t('navMain.systems')} / {t('navMain.bpla')}
-        </p>
+      <div className="w-full flex flex-col gap-5 desktop:gap-6">
+        <Paragraph
+          className="font-light"
+          variant="grey"
+          text={`${t('navMain.systems')} / ${t('navMain.bpla')}`}
+        />
         <SectionTitle
           title={`${t('systems.bpla.hero.title')}`}
-          className="text-left w-full"
+          className="text-left text-[37px] w-full"
         />
-        <p className="font-light uppercase max-w-[1011px] bg-[linear-gradient(90.79deg,#F5F5F5_0.08%,#FDFFFF_100%)] bg-clip-text text-transparent text-[24px]">
-          {t('systems.bpla.hero.description')}
-        </p>
+        <Paragraph variant="grey" text={t('systems.bpla.hero.description')} />
       </div>
-      <img
-        src="/images/systems/bpla/d_bpla_specifications@1x.png"
-        className="my-[82px]"
-        alt="Bpla pegasus arms 25"
-        srcSet="/images/systems/bpla/d_bpla_specifications@1x.png 1x, /images/systems/bpla/d_bpla_specifications@2x.png 2x"
-        loading="lazy"
-        decoding="async"
-      />
+      <picture>
+        {/* MOBILE */}
+        <source
+          media="(max-width: 639px)"
+          srcSet="
+      /images/systems/bpla/m_bpla_specifications@1x.png 1x,
+      /images/systems/bpla/m_bpla_specifications@2x.png 2x
+    "
+        />
+
+        {/* TABLET */}
+        <source
+          media="(max-width: 1133px)"
+          srcSet="
+      /images/systems/bpla/t_bpla_specifications@1x.png 1x,
+      /images/systems/bpla/t_bpla_specifications@2x.png 2x
+    "
+        />
+
+        {/* DESKTOP (default) */}
+        <img
+          src="/images/systems/bpla/d_bpla_specifications@1x.png"
+          srcSet="/images/systems/bpla/d_bpla_specifications@2x.png 2x"
+          alt="Bpla pegasus arms 25"
+          loading="lazy"
+          decoding="async"
+          className="w-full"
+        />
+      </picture>
       <div className="w-full flex flex-col justify-end gap-5 items-end">
         {t('systems.bpla.hero.paragraphs', { returnObjects: true }).map(
           (text: string, index: number) => (
