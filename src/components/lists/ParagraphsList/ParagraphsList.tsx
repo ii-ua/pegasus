8bc2dd19ae040af02409000dac62629fcc8a5709
Yaroslav Khakhuda <hahyda14@gmail.com>
2025-11-18 14:23:36 +0200

    feat(CompositionSection, ParagraphsList): enhance layout and styling for improved readability

--- src/components/lists/ParagraphsList/ParagraphsList.tsx
+++ src/components/lists/ParagraphsList/ParagraphsList.tsx
@@ -12,16 +12,17 @@ export const ParagraphsList = ({
   return (
     <ul
       className={cn(
-        `flex flex-col list-disc w-full text-white text-[14px]`,
+        `flex flex-col w-full text-[#D9D9D9] text-[16px]`,
         className,
       )}
     >
       {paragraphs.map((paragraph, index) => (
         <li
-          className="py-5 border-b-2 border-dashed w-full border-[#939393] last:border-none first:pt-0"
+          className="py-3.5 tablet:py-4 desktop:py-5 flex gap-3.5  border-b-2 border-dashed items-center border-[#5A5A5A] last:border-none last:pb-0 first:pt-0"
           key={index}
         >
-          <Paragraph className="font-medium" text={paragraph} />
+          <div className=" w-[4px] h-[4px] flex-shrink-0 rounded-full bg-[#D9D9D9]" />
+          <Paragraph variant="grey" text={paragraph} />
         </li>
       ))}
     </ul>
--- src/modules/systems/bpak/CompositionSection/CompositionSection.tsx
+++ src/modules/systems/bpak/CompositionSection/CompositionSection.tsx
@@ -7,28 +7,28 @@ export const CompositionSection = () => {
   const { t } = useTranslation()
   return (
     <SectionContainer as="section" className="flex  gap-6 flex-col">
-      <div className="flex">
+      <div className="flex flex-col tablet:flex-row gap-6">
         <SectionTitle
           title={`${t('systems.bpak.composition.title')}`}
           className="text-left max-w-[852px]"
         />
-        <div className="flex flex-col flex-1/3 justify-between pb-[31px]">
+        <div className="flex flex-col flex-1/3 justify-center gap-5">
           {t('systems.bpak.composition.paragraphs', {
             returnObjects: true,
           }).map((paragraph: string, index: number) => (
-            <Paragraph key={index} text={paragraph} />
+            <Paragraph key={index} variant="grey" text={paragraph} />
           ))}
         </div>
       </div>
-      <div className="flex gap-6 items-start">
+      <div className="flex flex-col tablet:flex-row gap-6 items-start">
         <ParagraphsList
-          className="flex-1"
+          className="flex-1 order-1 tablet:order-0"
           paragraphs={t('systems.bpak.composition.items', {
             returnObjects: true,
           })}
         />
         <img
-          className="flex-1 w-max-[665px] h-max-[452px] object-contain"
+          className="flex-1 w-max-[665px] h-max-[452px] object-contain order-0 tablet:order-1"
           src="/images/systems/bpka/d_bpka_composition@1x.jpg"
           alt={t('systems.bpak.composition.title')}
           srcSet="/images/systems/bpka/d_bpka_composition@1x.jpg 1x, /images/systems/bpka/d_bpka_composition@2x.jpg 2x"
