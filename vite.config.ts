8af254c632767d3d264bd12f1f8b4969af66afc6
Yaroslav Khakhuda <hahyda14@gmail.com>
2025-11-19 16:58:47 +0200

    refactor(vite): reorganize plugin configuration and remove unused imports

--- vite.config.ts
+++ vite.config.ts
@@ -4,47 +4,46 @@ import viteReact from '@vitejs/plugin-react'
 import viteTsConfigPaths from 'vite-tsconfig-paths'
 import tailwindcss from '@tailwindcss/vite'
 import svgr from 'vite-plugin-svgr'
-import path from 'node:path'
-import url from 'node:url'
-import { nitro } from 'nitro/vite'
-
-const _dirname = path.dirname(url.fileURLToPath(import.meta.url))
+import netlify from '@netlify/vite-plugin-tanstack-start'
 
 const config = defineConfig({
+  server: {
+    preset: 'netlify',
+  },
   plugins: [
     // this is the plugin that enables path aliases
-    nitro(),
+    tanstackStart(),
     viteTsConfigPaths({
       projects: ['./tsconfig.json'],
     }),
     tailwindcss(),
 
-    tanstackStart(),
     viteReact(),
-    // svgr({
-    //   svgrOptions: {
-    //     svgo: true,
-    //     svgoConfig: {
-    //       plugins: [
-    //         {
-    //           name: 'preset-default',
-    //           params: {
-    //             overrides: {
-    //               removeUnknownsAndDefaults: false,
-    //               removeUselessDefs: false,
-    //               cleanupIDs: false,
-    //               convertShapeToPath: false,
-    //               removeViewBox: false,
-    //               // не чіпаємо fill/stroke, залишаємо градієнти
-    //               removeAttrs: false,
-    //             },
-    //           },
-    //         },
-    //       ],
-    //     },
-    //   },
-    //   include: '**/*.svg?react',
-    // }),
+    svgr({
+      svgrOptions: {
+        svgo: true,
+        svgoConfig: {
+          plugins: [
+            {
+              name: 'preset-default',
+              params: {
+                overrides: {
+                  removeUnknownsAndDefaults: false,
+                  removeUselessDefs: false,
+                  cleanupIDs: false,
+                  convertShapeToPath: false,
+                  removeViewBox: false,
+                  // не чіпаємо fill/stroke, залишаємо градієнти
+                  removeAttrs: false,
+                },
+              },
+            },
+          ],
+        },
+      },
+      include: '**/*.svg?react',
+    }),
+    netlify(),
   ],
 })
 
