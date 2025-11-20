f4423af2c1fe5b9fd18589ba0d23e1c16a3ac33c
Yaroslav Khakhuda <hahyda14@gmail.com>
2025-11-20 16:45:26 +0200

    feat(blog): add social icons component and adjust layout spacing

--- src/modules/blog/ArticleOneSection/ArticleOneSection.tsx
+++ src/modules/blog/ArticleOneSection/ArticleOneSection.tsx
@@ -1,4 +1,5 @@
 import SectionContainer from '@/components/container/SectionContainer'
+import { SocialIcons } from '@/components/lists/SocialIcons/SocialIcons'
 import { Paragraph, SectionTitle } from '@/components/text'
 import { useTranslation } from 'react-i18next'
 
@@ -7,7 +8,7 @@ export const ArticleOneSection = () => {
   return (
     <SectionContainer
       as="section"
-      className="relative  pt-[90px] tablet:pt-24 desktop:pt-[122px] overflow-hidden pb-0 tablet:pb-0 desktop:pb-0"
+      className="relative flex pt-[90px] tablet:pt-24 desktop:pt-[122px] overflow-hidden pb-0 tablet:pb-0 desktop:pb-0 gap-6"
     >
       <div className="flex flex-col gap-6 max-w-3xl desktop:max-w-[1011px] border-dashed border-b-2 border-b-[#5A5A5A] pb-6">
         <SectionTitle title={`${t('blog.title')}`} className="text-left" />
