3ec7be618f87a896bfb0f17743e955a93557ff2d
Yaroslav Khakhuda <hahyda14@gmail.com>
2025-11-20 14:07:50 +0200

    feat(terms): enhance terms section with additional paragraphs and translations

--- src/modules/security/TermsSection/TermsSection.tsx
+++ src/modules/security/TermsSection/TermsSection.tsx
@@ -1,5 +1,5 @@
 import SectionContainer from '@/components/container/SectionContainer'
-import { SectionTitle } from '@/components/text'
+import { Paragraph, SectionTitle } from '@/components/text'
 import { useTranslation } from 'react-i18next'
 
 export const TermsSection = () => {
@@ -7,12 +7,90 @@ export const TermsSection = () => {
   return (
     <SectionContainer
       as="section"
-      className="relative pt-[90px] tablet:pt-24 desktop:pt-[122px] overflow-hidden"
+      className="relative  pt-[90px] tablet:pt-24 desktop:pt-[122px] overflow-hidden"
     >
-      <SectionTitle
-        title={`${t('terms.title')}`}
-        className="text-left max-w-[600px] desktop:max-w-[760px]"
-      />
+      <div className="flex flex-col gap-6 max-w-3xl desktop:max-w-[1011px]">
+        <SectionTitle
+          title={`${t('terms.title')}`}
+          className="text-left max-w-[600px] text-[36px] desktop:max-w-[760px]"
+        />
+        <Paragraph variant="light" text={t('terms.access.title')} />
+        {t('terms.access.paragraphs', { returnObjects: true }).map(
+          (paragraph: string, index: number) => (
+            <Paragraph
+              key={`access-${index}`}
+              variant="grey"
+              text={paragraph}
+            />
+          ),
+        )}
+        <Paragraph variant="light" text={t('terms.contents.title')} />
+        {t('terms.contents.paragraphs', { returnObjects: true }).map(
+          (paragraph: string, index: number) => (
+            <Paragraph
+              key={`contents-${index}`}
+              variant="grey"
+              text={paragraph}
+            />
+          ),
+        )}
+        <Paragraph variant="light" text={t('terms.accessWebsite.title')} />
+        {t('terms.accessWebsite.paragraphs', { returnObjects: true }).map(
+          (paragraph: string, index: number) => (
+            <Paragraph
+              key={`accessWebsite-${index}`}
+              variant="grey"
+              text={paragraph}
+            />
+          ),
+        )}
+        <Paragraph variant="light" text={t('terms.relationship.title')} />
+        {t('terms.relationship.paragraphs', { returnObjects: true }).map(
+          (paragraph: string, index: number) => (
+            <Paragraph
+              key={`relationship-${index}`}
+              variant="grey"
+              text={paragraph}
+            />
+          ),
+        )}
+        <Paragraph variant="light" text={t('terms.limitation.title')} />
+        {t('terms.limitation.paragraphs', { returnObjects: true }).map(
+          (paragraph: string, index: number) => (
+            <Paragraph
+              key={`limitation-${index}`}
+              variant="grey"
+              text={paragraph}
+            />
+          ),
+        )}
+        <Paragraph variant="light" text={t('terms.intellectual.title')} />
+        {t('terms.intellectual.paragraphs', { returnObjects: true }).map(
+          (paragraph: string, index: number) => (
+            <Paragraph
+              key={`intellectual-${index}`}
+              variant="grey"
+              text={paragraph}
+            />
+          ),
+        )}
+        <Paragraph variant="light" text={t('terms.changesTerms.title')} />
+        {t('terms.changesTerms.paragraphs', { returnObjects: true }).map(
+          (paragraph: string, index: number) => (
+            <Paragraph
+              key={`changesTerms-${index}`}
+              variant="grey"
+              text={paragraph}
+            />
+          ),
+        )}
+
+        <Paragraph
+          className="text-[#939393]"
+          variant="light"
+          text={t('terms.lastUpdate')}
+        />
+      </div>
     </SectionContainer>
   )
 }
