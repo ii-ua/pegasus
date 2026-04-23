ff889d63e8dd47d6fcc9f9962f991b8e917d7e8b
Yaroslav Khakhuda <hahyda14@gmail.com>
2026-04-23 18:30:47 +0300

    fix(education): correct academic and flight training hours in education section

--- src/common/localization/translation/en/systems/education.ts
+++ src/common/localization/translation/en/systems/education.ts
@@ -11,8 +11,8 @@ const education = {
       'The program combines a theoretical course and hands-on training in highly realistic, combat-like conditions.',
     ],
     items: [
-      '48 academic hours of theory and practice',
-      '48 hours of flight training',
+      '36 academic hours of theory and practice',
+      '36 hours of flight training',
       'Operation of the PEGASUS ARMS 25 strike UAS with various combat payload configurations',
       'Accomplishment of battlefield missions and work in harsh weather conditions',
       'Counter-UAS operations and employment of EW/ES and counter-UAS assets',
